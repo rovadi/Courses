9f98d1c732025168d5732ad9441ffd31b1c1e953
Roberto Valcárcel <roberto.valcarcel@beezy.net>
2021-03-05 21:49:59 +0100

    format eslint file with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
 	env: {
 		browser: true,
-		es2021: true
+		es2021: true,
 	},
 	parser: '@babel/eslint-parser',
 	extends: ['airbnb-base', 'prettier'],
 	parserOptions: {
-		requireConfigFile: false
+		requireConfigFile: false,
 	},
-	plugins: ['prettier']
+	plugins: ['prettier'],
 };
